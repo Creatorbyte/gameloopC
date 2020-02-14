130ac35e266c99313a63925b4d7eae7e3db83260
Christian Clark <46736965+Creatorbyte@users.noreply.github.com>
2020-02-14 16:49:37 -0600

    Just played around and made a new demo
    Modified

--- sdlgame/src/main.c
+++ sdlgame/src/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
 #include <SDL2/SDL.h>
 #include "./constants.h"
 #include "main.h"
+
+float delta_time = 0;
+int nextX = 0, nextY = 0;
 
 struct game_object {
     float x;
@@ -10,63 +16,74 @@ struct game_object {
     float height;
     float vel_x;
     float vel_y;
-} ball, paddle;
+} ball, wall;
 
 // the setup that is run once at the program start
 void setup(void) {
+    srand(time(0));
     ball.width = 15;
     ball.height = 15;
     ball.x = 20;
     ball.y = 20;
-    ball.vel_x = 300;
-    ball.vel_y = 300;
+    ball.vel_x = 30;
+    ball.vel_y = 30;
 
-    paddle.width = 100; 
-    paddle.height = 20;
-    paddle.x = (WINDOW_WIDTH / 2) - (paddle.width / 2);
-    paddle.y = WINDOW_HEIGHT - 40;
-    paddle.vel_x = 0;
-    paddle.vel_y = 0;
+    wall.width = WINDOW_WIDTH; 
+    wall.height = WINDOW_HEIGHT/5;
+    wall.x = 0;//(WINDOW_WIDTH / 2) - (wall.width / 2);
+    wall.y =WINDOW_HEIGHT - 100;
+    wall.vel_x = 0;
+    wall.vel_y = 0;
 }
 
 // the update that happens 60 times per second
 void update(void) {
     // Waste some time / sleep until we reach the frame target time
     while (!SDL_TICKS_PASSED(SDL_GetTicks(), last_frame_time + FRAME_TARGET_TIME));
-
     // Get a delta time factor converted to seconds to be used to update my objects
-    float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
-
+    delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
     // Store the milliseconds of the current frame
     last_frame_time = SDL_GetTicks();
 
-    // Check for ball collision with the walls
-    if (ball.x + ball.width > WINDOW_WIDTH || ball.x < 0)
-        ball.vel_x = -ball.vel_x;
-    if (ball.y < 0)
-        ball.vel_y = -ball.vel_y;
-
-    // Check for ball collision with the paddle
-    if (ball.y + ball.height >= paddle.y && ball.x + ball.width > paddle.x && ball.x < paddle.x + paddle.width)
-        ball.vel_y = -ball.vel_y;
-
     // Check for game over
-    if (ball.y + ball.height > WINDOW_HEIGHT)
-        game_is_running = FALSE;
+    //if (ball.y + ball.height > WINDOW_HEIGHT)
+        //game_is_running = FALSE;
 
-    // update ball position
-    ball.x += ball.vel_x * delta_time;
-    ball.y += ball.vel_y * delta_time;
 
-    // update paddle position
-    paddle.x += paddle.vel_x * delta_time;
-    paddle.y += paddle.vel_y * delta_time;
+    // check wall collisions
+    if (ball.x+ball.vel_x < 0 || ball.x+ball.vel_x > WINDOW_WIDTH-ball.width) {
+        //bounce on side!
+        ball.vel_x *= -0.8;
+    } else {
+        ball.x += ball.vel_x*0.98;
+    }
+    
+    // move ball up/down
+    if (ball.y+ball.vel_y < 0 || ball.y+ball.vel_y > wall.y-ball.height) {
+     // bounce on floor/ceiling
+        ball.vel_y = ball.vel_y * -0.8;
+    }
+    
+    //if bounce was too small -> bump into air
+    if (sqrt(ball.vel_y * ball.vel_y) < 0.01) {
+        ball.vel_x = rand()/500;
+        ball.vel_y = rand()/500;
+    } else {
+        ball.y += ball.vel_y;
+    }
+    
+    // gravity!
+    ball.vel_y += 0.2;
 }
 
 // rednder draws to the screen
 void render(void) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);// self explanitory
-    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 10);// self explanitory
+
+    SDL_Rect BG = {0,0,WINDOW_WIDTH,WINDOW_HEIGHT};
+
+    SDL_RenderFillRect(renderer, &BG);
+    //SDL_RenderClear(renderer);
 
     // Draw a rectangle
     SDL_Rect ball_rect = {
@@ -78,15 +95,15 @@ void render(void) {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderFillRect(renderer, &ball_rect);
 
-    // Render the left paddle
-    SDL_Rect paddle_rect = {
-        (int)paddle.x,
-        (int)paddle.y,
-        (int)paddle.width,
-        (int)paddle.height
+    // Render the left wall
+    SDL_Rect wall_rect = {
+        (int)wall.x,
+        (int)wall.y,
+        (int)wall.width,
+        (int)wall.height
     };
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &paddle_rect);
+    SDL_RenderFillRect(renderer, &wall_rect);
 
     SDL_RenderPresent(renderer);
 }
@@ -100,24 +117,16 @@ void process_input(void) {
     switch (event.type) {
         case SDL_QUIT:
             game_is_running = FALSE;
-            break;
+            break; 
         case SDL_KEYDOWN:
             if (event.key.keysym.sym == SDLK_ESCAPE)
                 game_is_running = FALSE;
-            if (event.key.keysym.sym == SDLK_LEFT)
-                paddle.vel_x = -400;
-            if (event.key.keysym.sym == SDLK_RIGHT)
-                paddle.vel_x = +400;
             break;
         case SDL_KEYUP:
-            if (event.key.keysym.sym == SDLK_LEFT)
-                paddle.vel_x = 0;
-            if (event.key.keysym.sym == SDLK_RIGHT)
-                paddle.vel_x = 0;
+            
             break;
     }
 }
-
 
 // the main game loop
 int main(int argc, char **argv) {
